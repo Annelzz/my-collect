3e39936a11b27c976e993f1b529e69b99d0b9d0c
Annelise <annelzz26@gmail.com>
2020-11-12 20:25:44 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(items)
+    x = 0
+    new_array = []
+    while x < items.length
+        new_array << yield(items[x])
+        x += 1
+    end    
+    items.collect do |thing|
+        thing.upcase
+        return new_array
+    end
+    items.collect do |anotherthing|
+    anotherthing.split(" ").first
+    end
+end
+    
